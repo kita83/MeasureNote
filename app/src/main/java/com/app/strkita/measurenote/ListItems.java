3ec977896c4bdac0c91799896763c9815a869081
Hiroaki Kitada <kita83@gmail.com>
2017-03-05 09:37:48 +0900

    タイマー制御実装

---
+++ app/src/main/java/com/app/strkita/measurenote/ListItems.java
@@ -0,0 +1,46 @@
+package com.app.strkita.measurenote;
+
+/**
+ * ListView表示用データ補助クラス
+ * Created by kitada on 2017/02/13.
+ */
+
+public class ListItems {
+
+    private int id;
+    private String body;
+    private int elapsedTime;
+    private int currentCount;
+    private int goalCount;
+    private int created;
+    private int updated;
+
+    public int getCurrentCount() {
+        return currentCount;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBody() {return body;}
+
+    public int getElapsedTime() {
+        return elapsedTime;
+    }
+
+    public int getGoalCount() {
+        return goalCount;
+    }
+
+    public int getCreated() {
+        return created;
+    }
+
+    public int getUpdated() {
+        return updated;
+    }
+}
