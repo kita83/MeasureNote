032749068eda88131d363ebad60844da1e6a819a
Hiroaki Kitada <kita83@gmail.com>
2017-03-12 10:11:44 +0900

    dialog fragment

--- app/src/main/java/com/app/strkita/measurenote/CountDialogFragment.java
+++ app/src/main/java/com/app/strkita/measurenote/CountDialogFragment.java
@@ -1,15 +1,12 @@
 package com.app.strkita.measurenote;
 
 import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.WindowManager;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 /**
@@ -21,33 +18,44 @@ public class CountDialogFragment extends DialogFragment {
 
     public CountDialogFragment() {}
 
+    static CountDialogFragment newInstance(int num) {
+        CountDialogFragment f = new CountDialogFragment();
+
+        // Supply num input as an argument.
+        Bundle args = new Bundle();
+        args.putInt("num", num);
+        f.setArguments(args);
+
+        return f;
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.input_count_dialog, null, false);
+        final View view = inflater.inflate(R.layout.input_count_dialog, null, false);
 
-        // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         builder.setTitle("目標文字数を設定");
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        // FIRE ZE MISSILES!
+                        EditText editText = (EditText) view.findViewById(R.id.dialog_edit);
                     }
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        // User cancelled the dialog
                     }
                 });
-
-
         builder.setView(view);
+        Dialog dialog = builder.create();
+
+        // ダイアログ外タップで消えないように設定
+        dialog.setCanceledOnTouchOutside(false);
 
 //        InputMethodManager imm = (InputMethodManager) getActivity().
 //                getSystemService(Context.INPUT_METHOD_SERVICE);
 //        imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, InputMethodManager.HIDE_NOT_ALWAYS);
-        // Create the AlertDialog object and return it
-        return builder.create();
+        return dialog;
     }
+
 }
