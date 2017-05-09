380805a7b0e4101097887f7160fd1b1157f0b949
Hiroaki Kitada <kita83@gmail.com>
2017-05-09 22:58:24 +0900

    設定用フラグメント作成

---
+++ app/src/main/java/com/app/strkita/measurenote/SettingFragment.java
@@ -0,0 +1,102 @@
+package com.app.strkita.measurenote;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.Collections;
+import java.util.Set;
+
+
+/**
+ * 設定用フラグメント
+ */
+public class SettingFragment extends PreferenceFragment
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    public SettingFragment() {}
+
+    // 変更イベントをActivityに通知する
+    public interface SettingFragmentListener {
+        void onSettingChanged();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // ファイル名を指定する
+        getPreferenceManager().setSharedPreferencesName(SettingPrefUtil.PREF_FILE_NAME);
+        // Preferencesの設定ファイルを指定
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        setTypefaceSummary(getPreferenceManager().getSharedPreferences());
+        setPrefixSummary(getPreferenceManager().getSharedPreferences());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(
+            SharedPreferences sharedPreferences, String key) {
+        // Activityを取得
+        Activity activity = getActivity();
+
+        // ActivityがSettingFragmentListenerを実装しているのであれば、通知する
+        if (activity instanceof SettingFragmentListener) {
+            SettingFragmentListener listener = (SettingFragmentListener)activity;
+
+            // Activityに変更通知
+            listener.onSettingChanged();
+        }
+
+        // サマリーに反映する
+        if (activity.getString(R.string.key_text_style).equals(key)) {
+            setTypefaceSummary(sharedPreferences);
+        }
+    }
+
+    private void setTypefaceSummary(SharedPreferences sharedPreferences) {
+        String key = getActivity().getString(R.string.key_text_style);
+
+        Preference preference = findPreference(key);
+
+        Set<String> selected = sharedPreferences.getStringSet(key, Collections.<String>emptySet());
+        preference.setSummary(TextUtils.join("/", selected.toArray()));
+    }
+
+    private void setPrefixSummary(SharedPreferences sharedPreferences) {
+        String key = getActivity().getString(R.string.key_file_name_prefix);
+
+        Preference preference = findPreference(key);
+
+        String prefix = sharedPreferences.getString(key, "");
+        preference.setSummary(prefix);
+    }
+}
---
+++ app/src/main/java/com/app/strkita/measurenote/SettingPrefUtil.java
@@ -0,0 +1,56 @@
+package com.app.strkita.measurenote;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * SharedPreferences設定用ユーティリティクラス
+ * Created by kitada on 2017/05/09.
+ */
+
+public class SettingPrefUtil {
+
+    // 保存先ファイル名
+    public static final String PREF_FILE_NAME = "settings";
+
+    private static final String KEY_TEXT_SIZE = "text.size";
+    public static final String TEXT_SIZE_LARGE = "large";
+    public static final String TEXT_SIZE_MEDIUM = "medium";
+    public static final String TEXT_SIZE_SMALL = "small";
+
+    private static final String KEY_SCREEN_REVERSE = "screen.reverse";
+
+    // Utilクラスのため、インスタンスを作成させない
+    private SettingPrefUtil() {}
+
+    // フォントサイズを取得する
+    public static float getFontSize(Context context) {
+        // SharedPreferencesを取得
+        SharedPreferences sp = context.getSharedPreferences(
+                PREF_FILE_NAME, Context.MODE_PRIVATE);
+        // 現在の設定値
+        String storedSize = sp.getString(KEY_TEXT_SIZE, TEXT_SIZE_MEDIUM);
+
+        // 設定値に応じて、実際のテキストサイズを返す
+        switch (storedSize) {
+            case TEXT_SIZE_LARGE:
+                return context.getResources().getDimension(
+                        R.dimen.settings_text_size_large);
+            case TEXT_SIZE_MEDIUM:
+                return context.getResources().getDimension(
+                        R.dimen.settings_text_size_medium);
+            case TEXT_SIZE_SMALL:
+                return context.getResources().getDimension(
+                        R.dimen.settings_text_size_small);
+            default:
+                return context.getResources().getDimension(
+                        R.dimen.settings_text_size_medium);
+        }
+    }
+
+    // 画面の明暗を反転するかどうか
+    public static boolean isScreenReverse(Context context) {
+        SharedPreferences sp = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+        return sp.getBoolean(KEY_SCREEN_REVERSE, false);
+    }
+}
